2938cb3e6046537531624cfd6104a472ccc2523d
Pap Dávid <papda@t-online.hu>
2015-08-06 13:32:37 +0200

    #40 Felhasználó kezeléshez szükséges entitások: Modified   application/sales-module/core/src/main/resources/META-INF/persistence.xml Modified   application/sales-module/core/src/test/resources/META-INF/persistence.xml Add        application/sales-module/core/src/main/java/hu/neuron/java/core/dao/RoleDAO.java Add        application/sales-module/core/src/main/java/hu/neuron/java/core/dao/UserDAO.java Add        application/sales-module/core/src/main/java/hu/neuron/java/core/entity/Role.java Add        application/sales-module/core/src/main/java/hu/neuron/java/core/entity/User.java Add        application/sales-module/core/src/test/java/core/RoleDaoTest.java Add        application/sales-module/core/src/test/java/core/UserDaoTest.java Add        documentation/sql scripts/user&role.sql
    Signed-off-by: Pap Dávid <papda@t-online.hu>

---
+++ application/sales-module/core/src/main/java/hu/neuron/java/core/entity/User.java
@@ -0,0 +1,96 @@
+package hu.neuron.java.core.entity;
+
+import java.util.Collection;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "User")
+@NamedQuery(name = "User.findUserByName", query = "SELECT u FROM User u  WHERE u.userName = :userName")
+public class User extends Base {
+
+	private static final long serialVersionUID = 1L;
+
+	private String name;
+
+	private String userId;
+
+	private String userName;
+
+	private String password;
+
+	private String email;
+
+	private String phoneNumber;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	@ManyToMany(fetch = FetchType.LAZY)
+	@JoinTable(name = "user_role_sw")
+	private Collection<Role> roles;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Collection<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Collection<Role> roles) {
+		this.roles = roles;
+	}
+
+	@Override
+	public String toString() {
+		return "User [userName=" + userName + ", password=" + password + ", getId()=" + getId() + "]";
+	}
+
+}
