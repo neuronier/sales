b3e68d2a4686b3b3fa50b5705074180598a0a3b0
Ráti Roland <rol.rati@gmail.com>
2015-08-07 11:26:31 +0200

    ügyfélhez tartozó szolgáltatások hozzáadva

---
+++ application/sales-module/business-api/src/main/java/hu/neuron/java/sales/service/ClientServiceRemote.java
@@ -0,0 +1,16 @@
+package hu.neuron.java.sales.service;
+
+import hu.neuron.java.sales.service.vo.ClientVO;
+
+import java.util.List;
+
+public interface ClientServiceRemote {
+	public ClientVO findClientByName(String name) throws Exception;
+
+	public void registrationClient(ClientVO clientVO) throws Exception;
+
+	public List<ClientVO> getClientList(int i, int pageSize, String sortField, int dir, String filter, String filterColumnName);
+
+	public void saveClient(ClientVO selectedClient);
+
+}
