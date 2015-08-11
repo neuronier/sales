a80d575a344ee9b364cde508f2c03092f896a250
Ráti Roland <rol.rati@gmail.com>
2015-08-11 10:33:49 +0200

    rendelés entitások hozzáadva

---
+++ application/sales-module/core/src/main/java/hu/neuron/java/core/entity/OrderProductType.java
@@ -0,0 +1,56 @@
+package hu.neuron.java.core.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+//Ez az entitás a rendeléshez szükséges Order és ProductType (még nem létezik)!!! közti kapcsolatot valósítja meg (Roli)
+@Entity
+@Table(name="OrderProductType")
+public class OrderProductType extends Base {
+
+	private static final long serialVersionUID = 1L;
+	
+	@ManyToOne(fetch = FetchType.LAZY, targetEntity = Order.class)
+	@JoinTable(name="order_producttype_sw")
+	private Long orderId;
+	
+	//TODO
+	/* MISSING ANNOTATIONS !!! */
+	private Long productTypeId;
+	
+	private int quantity;
+
+	public Long getOrderId() {
+		return orderId;
+	}
+
+	public void setOrderId(Long orderId) {
+		this.orderId = orderId;
+	}
+
+	public Long getProductTypeId() {
+		return productTypeId;
+	}
+
+	public void setProductTypeId(Long productTypeId) {
+		this.productTypeId = productTypeId;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	@Override
+	public String toString() {
+		return "OrderProductType [orderId=" + orderId + ", productTypeId="
+				+ productTypeId + ", quantity=" + quantity + "]";
+	}
+	
+}
---
+++ application/sales-module/core/src/main/java/hu/neuron/java/core/entity/OrderUser.java
@@ -0,0 +1,56 @@
+package hu.neuron.java.core.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+
+//Ez az entitás a rendeléshez szükséges Order és User közti kapcsolatot valósítja meg (Roli)
+@Entity
+@Table(name="OrderUser")
+public class OrderUser extends Base {
+
+	private static final long serialVersionUID = 1L;
+	
+	@ManyToOne(fetch = FetchType.LAZY, targetEntity = Order.class)
+	@JoinTable(name="order_user_sw")
+	private Long orderId;
+	
+	@ManyToOne(fetch = FetchType.LAZY, targetEntity = User.class)
+	@JoinTable(name="order_user_sw")
+	private Long userId;
+	
+	private boolean complete;
+
+	public Long getOrderId() {
+		return orderId;
+	}
+
+	public void setOrderId(Long orderId) {
+		this.orderId = orderId;
+	}
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public boolean isComplete() {
+		return complete;
+	}
+
+	public void setComplete(boolean complete) {
+		this.complete = complete;
+	}
+
+	@Override
+	public String toString() {
+		return "OrderUser [orderId=" + orderId + ", userId=" + userId
+				+ ", complete=" + complete + "]";
+	}
+}
