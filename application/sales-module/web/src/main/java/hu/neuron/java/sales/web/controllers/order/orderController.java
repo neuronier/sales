2f5d908d6ebb3055c93cdfc30c2c1c7fb2977d15
Ráti Roland <rol.rati@gmail.com>
2015-08-16 20:59:46 +0200

    rendelés funckió bővítve, jelenleg is fejlesztés alatt

--- application/sales-module/web/src/main/java/hu/neuron/java/sales/web/controllers/order/orderController.java
+++ application/sales-module/web/src/main/java/hu/neuron/java/sales/web/controllers/order/orderController.java
@@ -1,6 +1,11 @@
 package hu.neuron.java.sales.web.controllers.order;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import hu.neuron.java.sales.service.OfferServiceRemote;
 import hu.neuron.java.sales.service.OrderServiceRemote;
+import hu.neuron.java.sales.service.vo.OfferVO;
 import hu.neuron.java.sales.service.vo.OrderVO;
 
 import javax.annotation.PostConstruct;
@@ -17,38 +22,71 @@ import org.primefaces.event.SelectEvent;
 public class orderController {
 
 	private LazyOrderModel lazyOrderModul;
-	
-	@EJB(name="OrderService", mappedName="OrderService")
+
+	@EJB(name = "OrderService", mappedName = "OrderService")
 	OrderServiceRemote orderService;
+
+	@EJB(name = "OfferService", mappedName = "OfferService")
+	OfferServiceRemote offerService;
 	
+	private final String[] status = {"Feldolgozas alatt", "Feldolgozva", "Kezbesitve"};
+	
+	private String selectedStatus;
+
 	private OrderVO selectedOrder;
-	
+
 	private OrderVO newOrder;
+
+	private List<OfferVO> offerVOs;
 	
+	private OfferVO selectedOffer;
+	
+	private String selectedOfferName;
+	
+	private List<String> helper;
+
 	private String orderId;
-	
+
 	@PostConstruct
-	public void init(){
+	public void init() {
 		setLazyOrderModul(new LazyOrderModel(orderService));
 	}
-	
-	public orderController(){
+
+	public orderController() {
 		super();
 	}
-	
-	
-	public void addNewOrderButtonAction(){
-		newOrder = new OrderVO();
+
+	//lekéri az a legördülű listához az jelenleg rendelhető offereket
+	public void offersInit() {
+		offerVOs = new ArrayList<OfferVO>();
+		helper = new ArrayList<String>();
+		offerVOs = offerService.findAll();
+		for (OfferVO ofv : offerVOs) {
+			helper.add(ofv.getName());
+		}
 	}
-	public void addNewOrder(){
-		newOrder.setStatus("Megrendelve");
-		
+
+	public void addNewOrderButtonAction() {
+		newOrder = new OrderVO();
+		offersInit();
+	}
+
+	public void addNewOrder() {
+		newOrder.setStatus(selectedStatus);
+		if (selectedOfferName != null) {
+			for (OfferVO ofv : offerVOs) {
+				if (selectedOfferName.equals(ofv.getName())) {
+					selectedOffer = ofv;
+				}
+			}
+			orderService.addOfferToOrder(selectedOffer, newOrder);
+		}
 		orderService.saveOrder(newOrder);
 	}
-	
-	public void onRowSelected(SelectEvent event){
+
+	public void onRowSelected(SelectEvent event) {
 		orderId = selectedOrder.getOrderId();
-		
+
 		FacesContext.getCurrentInstance().addMessage(
 				null,
 				new FacesMessage(FacesMessage.SEVERITY_INFO, "Info",
@@ -85,6 +123,50 @@ public class orderController {
 
 	public void setOrderId(String orderId) {
 		this.orderId = orderId;
+	}
+
+	public List<OfferVO> getOfferVOs() {
+		return offerVOs;
+	}
+
+	public void setOfferVOs(List<OfferVO> offerVOs) {
+		this.offerVOs = offerVOs;
+	}
+
+	public List<String> getHelper() {
+		return helper;
+	}
+
+	public void setHelper(List<String> helper) {
+		this.helper = helper;
+	}
+
+	public OfferVO getSelectedOffer() {
+		return selectedOffer;
+	}
+
+	public void setSelectedOffer(OfferVO selectedOffer) {
+		this.selectedOffer = selectedOffer;
+	}
+
+	public String getSelectedOfferName() {
+		return selectedOfferName;
+	}
+
+	public void setSelectedOfferName(String selectedOfferName) {
+		this.selectedOfferName = selectedOfferName;
+	}
+
+	public String[] getStatus() {
+		return status;
+	}
+
+	public String getSelectedStatus() {
+		return selectedStatus;
+	}
+
+	public void setSelectedStatus(String selectedStatus) {
+		this.selectedStatus = selectedStatus;
 	}
 
 }
