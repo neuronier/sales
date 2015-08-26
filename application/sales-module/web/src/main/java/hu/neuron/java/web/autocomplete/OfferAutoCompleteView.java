24f77e4c39dff55b033a5010d05783a8e8469c38
Ráti Roland <rol.rati@gmail.com>
2015-08-26 23:49:03 +0200

    Sell felület v.0.1.2

--- application/sales-module/web/src/main/java/hu/neuron/java/web/autocomplete/OfferAutoCompleteView.java
+++ application/sales-module/web/src/main/java/hu/neuron/java/web/autocomplete/OfferAutoCompleteView.java
@@ -1,24 +1,47 @@
 package hu.neuron.java.web.autocomplete;
 
+import hu.neuron.java.sales.service.ClientServiceRemote;
+import hu.neuron.java.sales.service.vo.ClientVO;
+
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
- 
+
 @ManagedBean
 public class OfferAutoCompleteView {
-       
-	private String offer; 
+
+	@EJB(name = "ClientService", mappedName = "ClientService")
+	ClientServiceRemote clientService;
+
+	private String offer;
+
+	private ClientVO selectedClient;
 
 	public List<String> completeText(String query) {
-        List<String> results = new ArrayList<String>();
-        for(int i = 0; i < 10; i++) {
-            results.add(query + i);
-        }
-         
-        return results;
-    }
-	
+		selectedClient = new ClientVO();
+		List<ClientVO> clients = clientService.findAll();
+		List<String> results = new ArrayList<String>();
+		query.toLowerCase();
+		for (ClientVO vos : clients) {
+			if (vos.getName().toLowerCase().startsWith(query)) {
+				results.add(vos.getName());
+			}
+		}
+
+		return results;
+	}
+
+	public void completeClientData() {
+		System.out.println("kiválasztott client" + offer);
+		try {
+			setSelectedClient(clientService.findClientByName(offer));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 	public String getOffer() {
 		return offer;
 	}
@@ -26,6 +49,12 @@ public class OfferAutoCompleteView {
 	public void setOffer(String offer) {
 		this.offer = offer;
 	}
-   
-	
+
+	public ClientVO getSelectedClient() {
+		return selectedClient;
+	}
+
+	public void setSelectedClient(ClientVO selectedClient) {
+		this.selectedClient = selectedClient;
+	}
 }
